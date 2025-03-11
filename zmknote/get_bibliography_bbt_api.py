c926f3c3b7e1cfc83de69ec093e2752bd095cc51
Scott Otterson <pcqzd21yhcv97s2i704qivdofgk6v9@bots.bitbucket.org>
2025-03-11 01:18:43 -0700

    test rfw.get_bibliograpy_bbt_api()

--- zmknote/get_bibliography_bbt_api.py
+++ zmknote/get_bibliography_bbt_api.py
@@ -3,72 +3,14 @@
 # See: https://www.perplexity.ai/search/can-pyzotero-get-a-bibliograph-GOBZjDewTvOsa0uKBnB2Tw
 # See: https://www.zotero.org/styles?q=idb%3Amodern-language-association
 
-import json
 import sys
+import pathlib as pl
 
-import requests
-
-def get_bibliography_bbt_api(item_citekey_bbt: str) -> str:
-    """Fetches bibliography entry from Better BibTeX using the provided citation key."""
-    # See: https://retorque.re/zotero-better-bibtex/exporting/json-rpc/index.html
-    # See: https://www.perplexity.ai/search/can-pyzotero-get-a-bibliograph-GOBZjDewTvOsa0uKBnB2Tw
-    # See: https://www.zotero.org/styles?q=idb%3Amodern-language-association"""
-    
-    url = "http://localhost:23119/better-bibtex/json-rpc"
-    headers = {
-        "Content-Type": "application/json",
-        "Accept": "application/json"
-    }
-    data = {
-        "jsonrpc": "2.0",
-        "method": "item.bibliography",
-        "params": [
-            [item_citekey_bbt],
-            {
-                "contentType": "text",
-                "id": "modern-language-association",
-                "locale": "en-US",
-                "quickCopy": False
-            }
-        ]
-    }
-
-    response = requests.post(url, headers=headers, data=json.dumps(data), timeout=5)
-    if response.status_code == 200:
-        result = response.json()
-        return result["result"]
-    
-    print(f"Error getting BBT biography: {response.status_code=}, {response.text=}", file=sys.stderr)
-    return ""
+# Define paths and credentials
+refwrangle_dir = pl.Path('~/ref/refwrangle').expanduser()
+sys.path.append(str(refwrangle_dir))
+import refwrangle as rfw  # Import your custom refwrangle module
 
 item_key = 'Dale19CompleteGuideBulgSquat'    
-bib = get_bibliography_bbt_api(item_key)
+bib = rfw.get_bibliography_bbt_api(item_key)
 print(f'{item_key=}: {bib=}')
-    
-# url = "http://localhost:23119/better-bibtex/json-rpc"
-# headers = {
-#     "Content-Type": "application/json",
-#     "Accept": "application/json"
-# }
-# data = {
-#     "jsonrpc": "2.0",
-#     "method": "item.bibliography",
-#     "params": [
-#         ["Dale19CompleteGuideBulgSquat"],
-#         {
-#             "contentType": "text",
-#             "id": "modern-language-association",
-#             "locale": "en-US",
-#             "quickCopy": False
-#         }
-#     ]
-# }
-
-# response = requests.post(url, headers=headers, data=json.dumps(data), timeout=5)
-
-# if response.status_code == 200:
-#     result = response.json()
-#     print(result["result"])
-# else:
-#     print(f"Error: {response.status_code}, {response.text}")    
-
