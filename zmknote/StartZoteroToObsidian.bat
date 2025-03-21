541176db21e639c217b6b2f553b8432e7011ddd6
Scott Otterson <pcqzd21yhcv97s2i704qivdofgk6v9@bots.bitbucket.org>
2025-03-21 12:17:20 -0700

    Add a .bat file for starting the zotero to obsidian receiver .exe

---
+++ zmknote/StartZoteroToObsidian.bat
@@ -0,0 +1,9 @@
+:: Starts the zotero item to obsidian note receiver .exe file
+:: You can min it to the taskbar and reopen it to check status.
+:: Handiest if you create a shortcut to this, and put it on your desktop
+
+@echo off
+cd /d C:\Users\scott\OneDrive\share\ref\refwrangle\zmknote\dist
+start "" zotero_to_obsidian_note_receiver.exe
+powershell -command "& {Add-Type -TypeDefinition '[DllImport(\"user32.dll\")] public static extern bool ShowWindow(System.IntPtr hWnd, int nCmdShow); [DllImport(\"kernel32.dll\")] public static extern IntPtr GetConsoleWindow(); public static void Minimize() { ShowWindow(GetConsoleWindow(), 6); }' -Name 'WinAPI' -Namespace 'WinAPI'; [WinAPI.WinAPI]::Minimize();}"
+
